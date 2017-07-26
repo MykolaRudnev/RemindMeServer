19ee1cb2e33ab3d4f76334b0cf58f0b317b7668b
Mykola Rudnev <RudnevMykola@gmail.com>
2017-07-26 19:46:09 +0300

    Preparing the server to get acquainted with the database

--- src/main/java/com/mikki/remindme/server/ApplicationInitializer.java
+++ src/main/java/com/mikki/remindme/server/ApplicationInitializer.java
@@ -9,20 +9,25 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
+
+//регистрацыя конфигурацыи в Spring контексте
 public class ApplicationInitializer  implements WebApplicationInitializer{
 
     private final static String DISPATCHER = "dispatcher";
-
 
     public void onStartup(ServletContext servletContext) throws ServletException {
 
 
         AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
         ctx.register(WebConfig.class);
-        servletContext.addListener(new ContextLoaderListener(ctx));
+        servletContext.addListener(new ContextLoaderListener(ctx)); //наш контекст который будем регистрировать в сервлет
 
 
-        ServletRegistration.Dynamic servlet = servletContext.addServlet(DISPATCHER, new DispatcherServlet(ctx));
+        ServletRegistration.Dynamic servlet = servletContext.addServlet(DISPATCHER, new DispatcherServlet(ctx));//имя на которое заригестрированно
+        //добавление сервлет контекста и
+        // замапить на определенный url
+
+
         servlet.addMapping("/");
         servlet.setLoadOnStartup(1);
 
--- src/main/java/com/mikki/remindme/server/config/WebConfig.java
+++ src/main/java/com/mikki/remindme/server/config/WebConfig.java
@@ -6,9 +6,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@Configuration
-@EnableWebMvc
-@ComponentScan("com.mikki.remindme.server")
+
+@Configuration //выполнить перед тем как развернуть контекст Spring
+@EnableWebMvc //включить WEB Mvc что даст возможность использование контройлеров REST контройлеров
+@ComponentScan("com.mikki.remindme.server")//где искать бины все классы , компоненты сервесы , репозитории
 public class WebConfig  extends WebMvcConfigurerAdapter {
 }
 
